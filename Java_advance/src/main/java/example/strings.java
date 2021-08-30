811442433afa3794752735ee4051eec11feae3bc
keorapetse32 <keorapetseletlape32@gmail.com>
2021-08-30 23:43:48 +0200

    fourth commit

---
+++ Java_advance/src/main/java/example/strings.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package example;
+
+/**
+ *
+ * @author keora
+ */
+public class strings {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append("Ja").append("va");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+        System.out.println(sb1.toString() == s1);
+        System.out.println(sb1.toString().equals(s1));
+    }
+}
